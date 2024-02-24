99ff0de4745d0a602a3d162446e1c9c490416eed
sandeshs0 <hellosandesh0@gmail.com>
2024-02-24 16:56:19 +0545

    Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-return const MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: LoginPage(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(
+        onTap: () {},
+      ),
 );
 
   }
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,29 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:fit_frenzyy/components/cusTextfield.dart';
+import 'package:fit_frenzyy/components/cusButton.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+  LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Controllers:
+  final usernameController=TextEditingController();
+  final passwordController=TextEditingController();
+
+  //signin
+  void doSignin(){
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(children: [
-            const SizedBox(height: 50),
+            const SizedBox(height: 40,),
             Image.asset(
               "lib/images/1.png",
-              height: 320,
               width: 320,
             ),
+            const SizedBox(height: 1,),
+            Image.asset(
+              "lib/images/welcomeillus.png",
+             height: 300,
+            ),
             // end of logo
-            // Welcome text
-            Text("LOGIN", style: TextStyle(
-                color: Colors.blueGrey,
-                fontSize: 40),
-              )
-            
-            ,
+            const SizedBox(height: 2),
+            // Username text field
+            MyTextField(controller: usernameController, hintText: "Username", obscureText: false),
+            const SizedBox(height: 20),
+            MyTextField(controller: passwordController, hintText: "Password", obscureText: true),
+            const SizedBox(height: 30),
+            MyButton(onTap: doSignin, text: "Signin"),
+            const SizedBox(height: 40,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an Account?"),
+                SizedBox(width: 4,),
+                Text("Register Now!",
+                style: TextStyle(color: Colors.indigo, fontWeight: FontWeight.bold),)
+              ],
+            )
+
           ],),
         ),
       ),
